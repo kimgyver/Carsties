e40deba7636ef320dddeafcad545db34cca70a1c
Jason <jason.kim@ambitai.com>
2024-02-21 10:48:35 +1300

    first commit

---
+++ src/AuctionService/Entities/Item.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuctionService.Entities
+{
+    [Table("Items")]
+    public class Item
+    {
+        public Guid Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Colour { get; set; }
+        public int Mileage { get; set; }
+        public string ImageUrl { get; set; }
+
+        // navigation properties (1:1)
+        public Auction Auction { get; set; }
+        public Guid AuctionId { get; set; }
+    }
+}
